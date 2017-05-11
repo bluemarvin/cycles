f1213c847089d5370db00c3ad6cc7fa598a9c4ee
Randall Barker <rbarker@mozilla.com>
2017-05-11 15:27:49 -0700

    add missing file

---
+++ js/fullscreen.js
@@ -0,0 +1,23 @@
+function toggleFullscreen(element) {
+  if (document.fullscreen || document.webkitIsFullScreen) {
+    if (document.exitFullscreen) {
+console.log("standard EXIT FULLSCREEN");
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+console.log("WebKit EXIT FULLSCREEN");
+      document.webkitExitFullscreen();
+    } else {
+console.log("No EXIT FULLSCREEN FOUND");
+    }
+  } else if (element) {
+    if (element.requestFullscreen) {
+console.log("standard FULLSCREEN");
+      element.requestFullscreen();
+    } else if (element.webkitRequestFullScreen) {
+console.log("WebKit FULLSCREEN");
+        element.webkitRequestFullScreen();
+    } else {
+console.log("No FULLSCREEN FOUND");
+    }
+  }
+}
